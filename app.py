b65011a734230cc0cd76929381872b601509b7c2
nayareth cristal <nayarethalbornoz@MacBook-Air-de-Nayareth.local>
2026-09-26 13:29:07 -0300

    Mejora SEO y configuracion para buscadores

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory("static", "robots.txt")
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory("static", "sitemap.xml")
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
